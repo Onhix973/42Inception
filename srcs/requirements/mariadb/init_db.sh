22109b699d2c19cb31fa03abbf605046508fd8a0
Thyanoui Lutz <tlutz@student.42lyon.fr>
2026-04-22 19:11:26 +0200

    still need to fix the db connection error but switched to wp-cli for a cleaner archi

--- srcs/requirements/mariadb/init_db.sh
+++ srcs/requirements/mariadb/init_db.sh
@@ -4,4 +4,4 @@ sed -i -e "s/MYSQL_NAME_PH/$(cat /run/secrets/mysql_name)/g" init.sql
 sed -i -e "s/MYSQL_USER_PH/$(cat /run/secrets/mysql_user)/g"  init.sql
 sed -i -e "s/MYSQL_PASSWORD_PH/$(cat /run/secrets/mysql_password)/g" init.sql
 
-mariadbd-safe --datadir=/var/lib/mysql --init-file=/init.sql --bind-address=0.0.0.0 --user=mysql
+mysqld --datadir=/var/lib/mysql --init-file=/init.sql --bind-address=0.0.0.0 --user=mysql
