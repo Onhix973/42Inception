08764168655b851bbb14ff1589b3f1167820db15
Thyanoui Lutz <tlutz@student.42lyon.fr>
2026-04-23 13:18:43 +0200

    WORKING

--- srcs/requirements/wordpress/entrypoint/setup_wordpress.sh
+++ srcs/requirements/wordpress/entrypoint/setup_wordpress.sh
@@ -7,25 +7,30 @@ if [ ! -d wp-content ]; then
 	wp core download --path="/var/www/html" --allow-root
 fi
 
+until mariadb -h mariadb -P 3306 -u tlutz -pbayle -e "SELECT 1" >/dev/null 2>&1; do
+  echo "Waiting for MariaDB..."
+  sleep 2
+done
+
 if [ ! -f wp-config.php ]; then 
 	wp config create \
 		--dbname="inception" \
 		--dbuser="tlutz" \
 		--dbpass="bayle" \
-		--dbhost="mariadb" \
+		--dbhost="mariadb:3306" \
 		--path="/var/www/html" \
 		--allow-root
 fi
 
-if [ ! wp core is-installed --allow-root ]; then
+# if [ ! wp core is-installed --allow-root ]; then
 	wp core install \
 		--url=tlutz.42.fr \
 		--title="Inception Blog" \
 		--admin_user="Elrond" \
 		--admin_password="fondcombe" \
 		--admin_email="elrond@example.com" \
-		--skip_email \
+		--skip-email \
 		--allow-root
-fi
+# fi
 
-exec php-fpm83 -F
+exec php-fpm83 -F --force-stderr
